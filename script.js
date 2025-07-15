b1cd8870c2a1eba30e92c4b43ab4e77538b6390d
rafaelsoutoo <rafaelsouto70017@gmail.com>
2025-07-14 22:43:47 -0300

    fix

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function toggleMode() {
         img.setAttribute('src', './assets/Avatar.png' )
     }
 }
+
+// Ajusta a imagem ao carregar a página conforme o modo atual
+window.addEventListener('DOMContentLoaded', function() {
+    const html = document.documentElement;
+    const img = document.querySelector("#profile img");
+    if(html.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-light.png');
+    } else {
+        img.setAttribute('src', './assets/Avatar.png');
+    }
+});
